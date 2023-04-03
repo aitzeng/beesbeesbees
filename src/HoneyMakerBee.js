5baa78c6593b3dcaf532c94766407565be59fe94
aitzeng <aitzeng@gmail.com>
2023-04-03 11:43:47 -0700

    Completed HoneyMaker constructor

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,20 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(age, color, food, job) {
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
